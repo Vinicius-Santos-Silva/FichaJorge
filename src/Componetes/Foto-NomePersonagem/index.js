a3dcd7f6ba8fa856b5fc6e16511aa4ba7bdc23b7
Vinicius <viniciussantos109@gmail.com>
2023-07-20 19:07:12 -0300

    Alterei o componente foto do personagem, alterei o icone da página, alterei a foto do personagem para uma de usuário, adicionei as fotos/icon de user. Parte de adicionar imagem feita, resta fazer salvar e preencher o campo

--- src/Componetes/Foto-NomePersonagem/index.js
+++ src/Componetes/Foto-NomePersonagem/index.js
@@ -1,8 +1,8 @@
 import './index.css'
 
-import { useState, useEffect } from "react";   
+import { useState, useEffect} from "react";   
 
-import FtPersonagem from '../../Imagens/Jorge.jpg'
+import FtUser from '../../Imagens/usuario.png'
 
 function Personagem(){
     
@@ -22,25 +22,51 @@ function Personagem(){
     
       alert('O nome digitado foi Salvo')
   
-  /*     
-    const mensagem = localStorage.getItem('NomePersonagem');
-    console.log(mensagem)
-      if(mensagem !== '' || null || undefined ){
-  
-      }
-      else{
-        alert('Nome alterado com sucesso!')
-      }  */
+
+
     };
+
+    function trocarImg(){
+      const imagemInput = document.getElementById('uploadImg');
+      const imagemPreview = document.getElementById('FtUsario');
+  
+      imagemInput.addEventListener('change', function(e) {
+        const arquivo = imagemInput.files[0];
+        const leitor = new FileReader();
+  
+        leitor.onload = function(e) {
+          imagemPreview.src = e.target.result;
+
+          localStorage.setItem('imagem', e.target.result);
+        }
+  
+        leitor.readAsDataURL(arquivo);
+      });
+    }
+
     
     return(
         <div id="CampoPersonagem">
         <strong> 
-          <input type="text" value={nome} onChange={(e) => {setNome(e.target.value)}} 
-          placeholder="Digite o nome do Personagem"/> 
+          <input 
+          type="text" 
+          value={nome} 
+          onChange={(e) => {setNome(e.target.value)}} 
+          placeholder="Digite o nome do Personagem"
+          /> 
           <button onClick={salvar}>Salvar</button>
         </strong>
-        <img src={FtPersonagem} alt="Foto do Personagem"/>
+
+        <div id='containerImg'>
+          <img src={FtUser} alt='uploadFt' id='FtUsario'/>
+
+          <input 
+          type='file' 
+          id='uploadImg' 
+          accept='image*/' 
+          onClick={trocarImg}/>
+
+        </div>
       </div>
     );
 }
